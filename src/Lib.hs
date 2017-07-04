5343e3985818dbc2699f6697de99042e4a66a4e9
Gabe Berke-Williams <gabebw@gabebw.com>
2017-07-03 21:46:00 -0700

    Extra works

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,28 +8,30 @@ import Text.Parsec.Char
 import Control.Applicative ((<$>), (<*>), (*>), pure)
 
 data Node = Node { expr :: String }
+          | Extra { expr :: String } -- extra stuff to be added on later; huge hack
 
 instance Show Node where
     show (Node s) = "Node " ++ (show s)
+    show (Extra s) = "Extra " ++ (show s)
 
 s = ": 1401927488:0;gcm 'You need jq too'"
 ss = ": 1401929181:0;gcm 'Extract methods'\n: 1401929212:0;gcm 'Tell people if they are unranked for a repo'"
+sss = ": 1401927488:0;gcm 'You need jq too'\n: 1401929040:0;gcm 'more tips\nx\ng'"
 
 evaluate :: String -> Either ParseError [Node]
 evaluate = parse parser "(unknown)"
 
--- A single line :
--- : 1401929181:0;gcm 'Extract methods'
--- A multi-line:
--- : 1401929040:0;gcm 'more tips\
--- \
--- g'
-
 parser :: Parser [Node]
-parser = sepBy lineParser endOfLine
+parser = do
+    sepBy (lineParser <|> extraParser) (char '\n')
 
 newItemStart :: Parser String
 newItemStart = string ": "
+
+extraParser :: Parser Node
+extraParser = do
+    expr <- many (noneOf "\n")
+    return $ Extra expr
 
 lineParser :: Parser Node
 lineParser = do
@@ -40,8 +42,3 @@ lineParser = do
     char ';'
     expr <- many (noneOf "\n")
     return $ Node expr
-
-    -- numberNode <- numberNodeParser
-    -- spaces
-    -- nodes <- many1 furtherExpressionParser
-    -- return $ [numberNode] ++ (concat nodes)

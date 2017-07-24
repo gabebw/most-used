4cba49f08e6766e6bbab6cdd209ad8f99e530a59
Gabe Berke-Williams <gabebw@gabebw.com>
2017-07-23 18:25:23 -0700

    Read history lines from stdin

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
 module Main where
 
 import MostUsed
+import Data.List
+import Data.Ord (comparing, Down(..))
 
 main :: IO ()
-main = putStrLn "evaluate"
+main = do
+    stdin <- getContents
+    case parseHistory stdin of
+      (Left err) -> putStrLn err
+      (Right items) -> print $ findMostUsed items
+
+findMostUsed :: [Item] -> [(Int, String)]
+findMostUsed items = reverseSort $
+        map toTuple $
+        groupBy sameCommand $
+        sortBy (comparing command) items
+    where
+        toTuple is@(Item c _:_) = (length is, c)
+        sameCommand (Item c1 _) (Item c2 _) = c1 == c2
+
+-- Faster than `reverse . sort`:
+-- https://ro-che.info/articles/2016-04-02-descending-sort-haskell
+reverseSort :: (Ord a) => [a] -> [a]
+reverseSort = sortBy (comparing Down)

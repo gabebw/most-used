c7625f1d4ec3d47a80078a238bbd59e297717859
Gabe Berke-Williams <gabebw@gabebw.com>
2017-07-29 11:18:05 -0700

    Don't put "parser" in the name of Parser methods
    Types will guide the way without this naming convention.

--- src/MostUsed.hs
+++ src/MostUsed.hs
@@ -35,44 +35,39 @@ instance Show Argument where
 
 -- Like parseHistory, but just skips over lines that can't be parsed
 parseHistory' :: String -> [Item]
-parseHistory' s = concat $ rights $ map (parse lineParser "(unknown)") $ lines s
+parseHistory' s = concat $ rights $ map (parse items "(unknown)") $ lines s
 
 parseHistory :: String -> Either String [Item]
 parseHistory s = concat <$> history (lines s)
 
 history :: [String] -> Either String [[Item]]
-history (s:ss) = case parse lineParser "(unknown)" s of
+history (s:ss) = case parse items "(unknown)" s of
                    (Left err) -> Left $ parseErrorPretty err
                    (Right x) -> (:) <$> Right x <*> history ss
 history [] = Right []
 
-lineParser :: Parser [Item]
-lineParser = do
+items :: Parser [Item]
+items = do
     some spaceChar
     some digitChar -- history number
     string "  "
-    items <- itemParser `sepBy` pipeParser
-    eof
-    return items
+    item `sepBy` pipe <* eof
 
-pipeParser :: Parser ()
-pipeParser = space >> char '|' >> space
+pipe :: Parser ()
+pipe = space >> char '|' >> space
 
-itemParser :: Parser Item
-itemParser = do
-    command <- some (satisfy (not . isSpace))
+item :: Parser Item
+item = do
+    command <- some $ satisfy (not . isSpace)
     space
-    arguments <- argumentsParser
+    arguments <- singleArgument `sepEndBy` separator
     return $ Item command arguments
-
-argumentsParser :: Parser [Argument]
-argumentsParser = singleArgumentParser `sepEndBy` separator
 
 separator :: Parser [String]
 separator = some (try escapedNewline <|> some spaceChar)
 
-singleArgumentParser :: Parser Argument
-singleArgumentParser =
+singleArgument :: Parser Argument
+singleArgument =
     DoubleQuoted <$> surroundedBy "\""
     <|> SingleQuoted <$> surroundedBy "'"
     <|> Backticks <$> surroundedBy "`"

54882910a931748dbfe565a5a3540ee122413105
Gabe Berke-Williams <gabebw@gabebw.com>
2017-08-01 22:17:13 -0700

    Fix tests to pass in the parser

--- test/MostUsed/MostUsedSpec.hs
+++ test/MostUsed/MostUsedSpec.hs
@@ -5,6 +5,7 @@ module MostUsed.MostUsedSpec
 
 import Data.List (intercalate)
 import MostUsed
+import MostUsed.Parser.Zsh as Zsh
 import Test.Hspec
 
 commandNumber :: String
@@ -30,68 +31,68 @@ spec = do
     describe "failures" $ do
         it "returns a pretty error message" $ do
             let s = "bad"
-            failures s `shouldBe` ["bad:1:1:\nunexpected 'b'\nexpecting white space\n"]
+            failures Zsh.items s `shouldBe` ["bad:1:1:\nunexpected 'b'\nexpecting white space\n"]
 
         it "does not return any successfully parsed commands" $ do
             let s = cmd "cmd 'a'"
-            failures s `shouldBe` []
+            failures Zsh.items s `shouldBe` []
 
     describe "successes" $ do
         it "ignores a single unparseable command" $ do
-            successes unparseableCommand `shouldBe` []
+            successes Zsh.items unparseableCommand `shouldBe` []
 
         it "ignores an unparseable command but parses the good one" $ do
             let s1 = unparseableCommand
             let s2 = cmd "cmd 'a'"
             let s = intercalate "\n" [s1, s2]
-            successes s `shouldBe` [Item "cmd" [SingleQuoted "a"]]
+            successes Zsh.items s `shouldBe` [Item "cmd" [SingleQuoted "a"]]
 
         describe "with a single item" $ do
             it "parses a command with no arguments" $ do
                 let s = cmd "cmd"
                 let result = Item "cmd" []
-                successes s `shouldBe` [result]
+                successes Zsh.items s `shouldBe` [result]
 
             it "parses a command with no arguments and a low number" $ do
                 let s = cmd2 "cmd"
                 let result = Item "cmd" []
-                successes s `shouldBe` [result]
+                successes Zsh.items s `shouldBe` [result]
 
             it "parses a command with one unquoted argument" $ do
                 let s = cmd "cmd arg"
                 let result = Item "cmd" [NotQuoted "arg"]
-                successes s `shouldBe` [result]
+                successes Zsh.items s `shouldBe` [result]
 
             it "parses a command with one single-quoted argument" $ do
                 let s = cmd "cmd 'a r g'"
                 let result = Item "cmd" [SingleQuoted "a r g"]
-                successes s `shouldBe` [result]
+                successes Zsh.items s `shouldBe` [result]
 
             it "parses a command with one double-quoted argument" $ do
                 let s = cmd "cmd \"a r g\""
                 let result = Item "cmd" [DoubleQuoted "a r g"]
-                successes s `shouldBe` [result]
+                successes Zsh.items s `shouldBe` [result]
 
             it "parses a command with one backtick-ed argument" $ do
                 let s = cmd "cmd `arg`"
                 let result = Item "cmd" [Backticks "arg"]
-                successes s `shouldBe` [result]
+                successes Zsh.items s `shouldBe` [result]
 
             it "parses a command with one $(argument)" $ do
                 let s = cmd "cmd $(arg x)"
                 let result = Item "cmd" [CommandSubstitution "arg x"]
-                successes s `shouldBe` [result]
+                successes Zsh.items s `shouldBe` [result]
 
             it "parses a command preceded by a backslash" $ do
                 let s = cmd "\\psql arg"
                 let result = Item "\\psql" [NotQuoted "arg"]
-                successes s `shouldBe` [result]
+                successes Zsh.items s `shouldBe` [result]
 
             it "parses a command with process substitution" $ do
                 let s = cmd "cmd <(one) <(two)"
                 let result = Item "cmd" [ProcessSubstitution "one"
                                         , ProcessSubstitution "two"]
-                successes s `shouldBe` [result]
+                successes Zsh.items s `shouldBe` [result]
 
             it "parses a command with a variety of arguments" $ do
                 let s = cmd "cmd one `two` '3 x' \"4 y\" $(arg)"
@@ -100,31 +101,31 @@ spec = do
                                         , SingleQuoted "3 x"
                                         , DoubleQuoted "4 y"
                                         , CommandSubstitution "arg"]
-                successes s `shouldBe` [result]
+                successes Zsh.items s `shouldBe` [result]
 
             it "parses a line with multiple items separated by a pipe" $ do
                 let s = cmd "c1 one |c2 `two`"
                 let result = [Item "c1" [NotQuoted "one"]
                              , Item "c2" [Backticks "two"]]
-                successes s `shouldBe` result
+                successes Zsh.items s `shouldBe` result
 
             it "parses a line with @" $ do
                 let s = cmd "echo @gabebw"
                 let result = [Item "echo" [NotQuoted "@gabebw"]]
 
-                successes s `shouldBe` result
+                successes Zsh.items s `shouldBe` result
 
             it "parses a line with $-quoting" $ do
                 let s = cmd "echo $' hello '"
                 let result = [Item "echo" [SingleQuoted " hello "]]
 
-                successes s `shouldBe` result
+                successes Zsh.items s `shouldBe` result
 
             it "parses a line with a variable" $ do
                 let s = cmd "echo $var"
                 let result = [Item "echo" [NotQuoted "$var"]]
 
-                successes s `shouldBe` result
+                successes Zsh.items s `shouldBe` result
 
         describe "with multiple items, each of which is on one line" $ do
             it "parses commands with a variety of arguments" $ do
@@ -137,13 +138,13 @@ spec = do
                                    , CommandSubstitution "arg"]
                 let i2 = Item "c2" [SingleQuoted "3 x"
                                    , DoubleQuoted "4 y"]
-                successes s `shouldBe` [i1, i2, Item "c3" []]
+                successes Zsh.items s `shouldBe` [i1, i2, Item "c3" []]
 
         it "can parse something with a bare escaped newline" $ do
             let s = cmd "fc -lDt 1 \\n"
             let item = Item "fc" [NotQuoted "-lDt", NotQuoted "1"]
 
-            successes s `shouldBe` [item]
+            successes Zsh.items s `shouldBe` [item]
 
         describe "with multiple items, not all of which are on one line" $ do
             it "parses commands with a variety of arguments" $ do
@@ -156,4 +157,4 @@ spec = do
                                    , CommandSubstitution "arg"]
                 let i2 = Item "c2" [SingleQuoted "3\\n x"
                                    , DoubleQuoted "4 \\ny"]
-                successes s `shouldBe` [i1, i2, Item "c3" []]
+                successes Zsh.items s `shouldBe` [i1, i2, Item "c3" []]

e9f072c4f1dc46407691bfa2c3c4cede16800729
Gabe Berke-Williams <gabebw@gabebw.com>
2017-08-06 21:31:26 -0700

    wip

--- src/MostUsed/Types.hs
+++ src/MostUsed/Types.hs
@@ -5,12 +5,36 @@ module MostUsed.Types
     , Shell(..)
     ) where
 
+import Data.List (sortBy)
+import Data.Ord (comparing)
+
 type CommandName = String
 
 data Shell = Zsh | Bash
 
 data Command = Command { commandName :: CommandName, arguments :: [Argument] }
             deriving (Show, Eq)
+
+data Result = Result
+    -- Has to be a commandName b/c almost no commands with all their arguments
+    -- will be dupes.
+    { resultName :: CommandName
+    , frequency :: Int
+    }
+
+value :: (Num a) => Result -> a
+value (Result n f) = fromIntegral $ length n * f
+
+-- Get value of nth percentile of the list
+percentileBoundary :: Int -> [a] -> Float
+percentileBoundary n xs = (fromIntegral ((length xs) * n)) / 100 + 0.5
+
+-- Get top nth percentile of a list
+topPercentile :: Int -> [Result] -> [Result]
+topPercentile n xs = filter (\r -> value r >= p) sorted
+    where
+        p = percentileBoundary n (map value xs)
+        sorted = sortBy (comparing value) xs
 
 data Argument = DoubleQuoted String
               | SingleQuoted String
